c0166a5ff1b0d4eb50fbd89cb6fda1e0732ff75e
lucas <lucasalves.souza01@gmail.com>
2024-03-22 13:18:51 -0300

    feat: add contrat implementation with methods to add place and retrieve places

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::{UnorderedSet, Vector};
 use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::env;
+use near_sdk::env::log_str;
 use near_sdk::{near_bindgen, AccountId};
+
+pub mod utils;
+pub use crate::utils::*;
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, Debug, Serialize)]
@@ -57,3 +62,69 @@ pub struct Contract {
     places: Vector<Place>,
     last_id: u64,
 }
+
+// Default state to use if no initialize method is called.
+// Define the default, which automatically initializes the contract
+impl Default for Contract {
+    fn default() -> Self {
+        Self {
+            // b"v" é um prefixador que vai ser usado como chave no store do contrato
+            owner: "wendersonpires.testnet".parse().unwrap(),
+            admins: UnorderedSet::new(b"s"),
+            places: Vector::new(b"v"),
+            last_id: 0,
+        }
+    }
+}
+
+#[near_bindgen]
+impl Contract {
+    #[init]
+    pub fn init(owner: AccountId, admins: Option<Vec<AccountId>>) -> Self {
+        assert!(!env::state_exists(), "Already initialized");
+
+        Self {
+            owner,
+            admins: account_vec_to_set(
+                if admins.is_some() {
+                    admins.unwrap()
+                } else {
+                    vec![]
+                },
+                b"s",
+            ),
+            places: Vector::new(b"v"),
+            last_id: 0,
+        }
+    }
+
+    pub fn add_place(&mut self, place: PlaceInput) {
+        let place_name = place.name.clone();
+        log_str(&format!("Adding new place: {place_name}"));
+
+        let new_place = Place {
+            id: self.last_id,
+            name: place.name,
+            address: place.address,
+            description: place.description,
+            place_type: place.place_type,
+            avarage_votes: 0,
+            votes_counter: 0,
+            votes: vec![],
+            pictures: place.pictures,
+        };
+
+        self.places.push(&new_place);
+        self.last_id += 1;
+    }
+
+    pub fn get_places(&self) -> Vec<Place> {
+        return self.places.to_vec();
+    }
+}
+
+
+// Tests in a separated file (see more here -> http://xion.io/post/code/rust-unit-test-placement.html)
+#[cfg(test)]
+#[path = "./tests.rs"]
+mod tests;
